e781e4576ca2750fa33f1779bdc733ae165dbeae
Marcin Kos <marcin.kos@student.wat.edu.pl>
2021-02-01 23:28:56 +0100

    Wykonano zadanie 3.3.1 –tekst statyczny

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+LiquidCrystal lcd (12,11,5,4,3,2);
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(16,2);
+  lcd.print("Kos");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+ void loop() {
+lcd.setCursor(0,1);
+    lcd.print(millis()/1000);
+    delay(500);
+ }
